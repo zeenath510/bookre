eccaabc9d462380cfb3905c6f45240f78af28c84
Sudharsan4449 <sudharsan4449@gmail.com>
2026-02-19 14:53:43 +0530

    Ensure HTTPS protocol in API_URL

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,4 +1,8 @@
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+let API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
+if (!API_URL.startsWith("http")) {
+    API_URL = `https://${API_URL}`;
+}
 
 export default API_URL;
